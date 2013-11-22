09b15a0755bde6677d037c6548bd325639df9904
Joel Schlosser <jbschlosser@gmail.com>
2013-11-22 01:02:28 -0500

    Removing the old window buffer

--- tests/check_mud.c
+++ tests/check_mud.c
@@ -4,13 +4,11 @@
 #include "mud_string_tests.h"
 #include "scrollback_buffer_tests.h"
 #include "utilities_tests.h"
-#include "window_buffer_tests.h"
 
 int main(void)
 {
     int number_failed;
-    SRunner *sr = srunner_create(make_window_buffer_suite());
-    srunner_add_suite(sr, make_utilities_suite());
+    SRunner *sr = srunner_create(make_utilities_suite());
     srunner_add_suite(sr, make_history_buffer_suite());
     srunner_add_suite(sr, make_mud_string_suite());
     srunner_add_suite(sr, make_scrollback_buffer_suite());
--- tests/window_buffer_tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef WINDOW_BUFFER_TESTS_H
-#define WINDOW_BUFFER_TESTS_H
-
-#include <check.h>
-
-Suite *make_window_buffer_suite(void);
-
-#endif
